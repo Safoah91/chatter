41aaf16792df00fe88eee422ad3c7edd148a051e
Safoah91 <lampteyanastasia@gmail.com>
2023-06-09 10:37:53 +0000

    comment line

--- src/pages/home/components/join.tsx
+++ src/pages/home/components/join.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-
-import { TfiStatsUp } from "react-icons/tfi";
-import { FaUsers } from "react-icons/fa";
-import { RiPagesLine } from "react-icons/ri";
+// import { TfiStatsUp } from "react-icons/tfi";
+// import { FaUsers } from "react-icons/fa";
+// import { RiPagesLine } from "react-icons/ri";
 
 const Join = () => {
   return <div>Join</div>;
