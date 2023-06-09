387d2b9d697394d50b3b29cd33f330f210c51cf6
Safoah91 <lampteyanastasia@gmail.com>
2023-06-09 13:55:11 +0000

    add home hero section

--- src/pages/home/components/hero.tsx
+++ src/pages/home/components/hero.tsx
@@ -1,5 +1,24 @@
+import Button from "components/button";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <section id="hero">
+        <div id="overlay" className="py-52">
+          <div className="max-w-4xl mx-auto">
+            <h1 className="text-5xl text-white leading-normal font-bold pb-5">
+              Welcome to Chatter: A Haven for Text-Based Content
+            </h1>
+            <p className="text-white text-lg md:max-w-lg mb-9">
+              Unleash the Power of Words, Connect with Like-minded Readers and
+              Writers
+            </p>
+            <Button type="primary-link" text="Get start" link="/" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Hero;
