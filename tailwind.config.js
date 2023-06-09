95d09b0872f4dcd45cffa49fc7af5a3863fcecaf
Safoah91 <lampteyanastasia@gmail.com>
2023-06-09 13:53:34 +0000

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue500: "#543EE0",
+        lightYellow: "#FFEDCC80",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
