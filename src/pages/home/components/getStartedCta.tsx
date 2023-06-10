3c0f905383ca99fa81d61b59307da32a381d9e40
Safoah91 <lampteyanastasia@gmail.com>
2023-06-10 11:07:13 +0000

    add home getStartedCta section

--- src/pages/home/components/getStartedCta.tsx
+++ src/pages/home/components/getStartedCta.tsx
@@ -1,5 +1,48 @@
+import Button from "components/button";
+
 const GetStartedCta = () => {
-  return <div>GetStartedCta</div>;
+  return (
+    <>
+      <section id="about" className="py-24">
+        <div className="max-w-6xl mx-auto md:flex md:gap-20">
+          <article className="w-[30%] rounded-full relative">
+            <div className="-mb-8">
+              <img
+                src="/assets/images/img1.png"
+                className="rounded-full"
+                alt="students studying"
+              />
+            </div>
+            <div className="ml-44 -mb-8">
+              <img
+                src="/assets/images/img2.png"
+                className="rounded-full"
+                alt="students studying"
+              />
+            </div>
+            <div>
+              <img
+                src="/assets/images/img.png"
+                className="rounded-full"
+                alt="students studying"
+              />
+            </div>
+          </article>
+          <article className="w-[70%]">
+            <h2 className="text-5xl font-bold max-w-2xl leading-tight">
+              Write, read and connect with great minds on chatter
+            </h2>
+            <p className="mt-6 mb-8 text-lg max-w-xl">
+              Share people your great ideas, and also read write-ups based on
+              your interests. connect with people of same interests and goals
+            </p>
+
+            <Button text="Get start" type="primary-link" />
+          </article>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default GetStartedCta;
