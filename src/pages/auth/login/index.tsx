9f47b57abd036f75feca91ef6781fde4aaceffec
Safoah91 <lampteyanastasia@gmail.com>
2023-06-21 20:25:56 +0000

    user login

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,9 +1,15 @@
 import Button from "components/button";
-import { FC, useState } from "react";
+import { FC, useCallback, useState } from "react";
 import AuthLayout from "..";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "react-query";
+import { loginUser } from "api/mutations/users";
+import { toast } from "react-toastify";
+import Cookies from "js-cookie";
 
 const Login: FC<{}> = () => {
   const [loginData, setLoginData] = useState<any>({});
+  const navigate = useNavigate();
 
   const handleChange = (e: any) => {
     setLoginData({
@@ -12,12 +18,54 @@ const Login: FC<{}> = () => {
     });
   };
 
+  const { mutateAsync, isLoading } = useMutation({
+    mutationFn: (body: any) => loginUser(body),
+    onSuccess: (response) => {
+      toast?.success("Welcome again!");
+      Cookies.set("accessToken", response.token);
+      navigate("/main");
+    },
+    onError: (e: any) => {
+      console.log(e);
+      toast?.error("Email and password wrong");
+    },
+  });
+
+  const handleSubmission = useCallback(
+    async (e: any) => {
+      e?.preventDefault();
+
+      if (loginData.email === undefined) {
+        return toast?.error("Email can't be empty");
+      }
+
+      if (loginData.password === undefined) {
+        return toast?.error("Password can't be empty");
+      }
+
+      if (
+        loginData.email.split("").filter((x: any) => x === "@").length !== -1 &&
+        loginData.email.indexOf(".") === -1
+      ) {
+        return toast?.error("Email is invalid");
+      }
+
+      mutateAsync({
+        ...loginData,
+      });
+    },
+    [loginData, mutateAsync]
+  );
+
   return (
     <>
       <AuthLayout>
         <>
           <h2 className="text-3xl font-bold text-center">Welcome back</h2>
-          <form className="flex items-center justify-center">
+          <form
+            onSubmit={handleSubmission}
+            className="flex items-center justify-center"
+          >
             <div>
               <div className="w-[36.7rem] my-6">
                 <label className="block text-gray-500 mb-3" htmlFor="email">
@@ -46,7 +94,11 @@ const Login: FC<{}> = () => {
                 />
               </div>
               <div className="w-[36.7rem] my-6">
-                <Button type="primary" text="Log in" />
+                <Button
+                  type="primary"
+                  text={isLoading ? "logging in..." : "Log in"}
+                  onClick={handleSubmission}
+                />
               </div>
             </div>
           </form>
