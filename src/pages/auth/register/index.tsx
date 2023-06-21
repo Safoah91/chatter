e4bba07bccec5852d1007fcf5e44705f6f00aa9e
Safoah91 <lampteyanastasia@gmail.com>
2023-06-21 20:25:48 +0000

    register uesrs

--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,9 +1,19 @@
-import { FC, useState } from "react";
+import { FC, useState, useCallback, useEffect } from "react";
 import AuthLayout from "..";
 import Button from "components/button";
+import { FcGoogle } from "react-icons/fc";
+import { BsLinkedin } from "react-icons/bs";
+import { useMutation } from "react-query";
+import { registerUser } from "api/mutations/users";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { auth, provider } from "firebase.config";
+import { signInWithPopup } from "firebase/auth";
 
 const Register: FC<{}> = () => {
   const [registerData, setRegisterData] = useState<any>({});
+  const [, setValue] = useState<any>();
+  const navigate = useNavigate();
 
   const handleChange = (e: any) => {
     setRegisterData({
@@ -12,19 +22,92 @@ const Register: FC<{}> = () => {
     });
   };
 
+  const { mutateAsync, isLoading } = useMutation({
+    mutationFn: (body: any) => registerUser(body),
+    onSuccess: () => {
+      toast?.success("Account created successfully!");
+      navigate("/login");
+    },
+    onError: (e: any) => {
+      console.log(e);
+      toast?.error("Could not log user in: ");
+    },
+  });
+
+  const handleSubmission = useCallback(
+    async (e: any) => {
+      e?.preventDefault();
+
+      if (registerData.firstName === undefined) {
+        return toast?.error("Email can't be empty");
+      }
+
+      if (registerData.lastName === undefined) {
+        return toast?.error("Email can't be empty");
+      }
+
+      if (registerData.joinAs === undefined) {
+        return toast?.error("select join as");
+      }
+
+      if (registerData.email === undefined) {
+        return toast?.error("Email can't be empty");
+      }
+
+      if (registerData.password === undefined) {
+        return toast?.error("Password can't be empty");
+      }
+
+      if (registerData.confirmPassword === undefined) {
+        return toast?.error("Confirm password can't be empty");
+      }
+
+      if (
+        registerData.email.split("").filter((x: any) => x === "@").length !==
+          -1 &&
+        registerData.email.indexOf(".") === -1
+      ) {
+        return toast?.error("Email is invalid");
+      }
+
+      if (registerData.password !== registerData.confirmPassword) {
+        return toast?.error("password and confirm password are not the same");
+      }
+
+      mutateAsync({
+        ...registerData,
+      });
+    },
+    [registerData, mutateAsync]
+  );
+
+  const handleClick = () => {
+    signInWithPopup(auth, provider).then((data: any) => {
+      setValue(data?.user?.email);
+      localStorage.setItem("email", data?.user?.email);
+    });
+  };
+
+  useEffect(() => {
+    setValue(localStorage.getItem("email"));
+  }, []);
+
   return (
     <>
       <AuthLayout>
         <>
-          <h2 className="text-3xl font-bold text-center">
+          <h2 className="text-3xl font-bold text-center -mt-9">
             Register as a Writer/Reader
           </h2>
-          <form className="flex items-center justify-center">
+          <form
+            onSubmit={handleSubmission}
+            className="flex items-center justify-center"
+          >
             <div>
-              <div className="flex gap-3 my-6">
+              <div className="flex gap-3 my-4">
                 <div className="w-72">
                   <label
-                    className="block text-gray-500 mb-3"
+                    className="block text-gray-500 mb-2"
                     htmlFor="firstName"
                   >
                     First name
@@ -41,7 +124,7 @@ const Register: FC<{}> = () => {
 
                 <div className="w-72">
                   <label
-                    className="block text-gray-500 mb-3"
+                    className="block text-gray-500 mb-2"
                     htmlFor="lastName"
                   >
                     Last name
@@ -57,14 +140,14 @@ const Register: FC<{}> = () => {
                 </div>
               </div>
               <div className="w-[36.7rem]">
-                <label className="block text-gray-500 mb-3" htmlFor="join">
+                <label className="block text-gray-500 mb-2" htmlFor="joinAs">
                   You are joining as?
                 </label>
                 <select
-                  name="join"
-                  value={registerData["join"] || ""}
+                  name="joinAs"
+                  value={registerData["joinAs"] || ""}
                   onChange={handleChange}
-                  id="join"
+                  id="joinAs"
                   className="w-full rounded-md border-gray-300 "
                 >
                   <option value="">Select...</option>
@@ -73,8 +156,8 @@ const Register: FC<{}> = () => {
                 </select>
               </div>
 
-              <div className="w-[36.7rem] my-6">
-                <label className="block text-gray-500 mb-3" htmlFor="email">
+              <div className="w-[36.7rem] my-4">
+                <label className="block text-gray-500 mb-2" htmlFor="email">
                   Email address
                 </label>
                 <input
@@ -86,8 +169,8 @@ const Register: FC<{}> = () => {
                   className="w-full rounded-md border-gray-300 placeholder:text-gray-300"
                 />
               </div>
-              <div className="w-[36.7rem] my-6">
-                <label className="block text-gray-500 mb-3" htmlFor="password">
+              <div className="w-[36.7rem] my-4">
+                <label className="block text-gray-500 mb-2" htmlFor="password">
                   Password
                 </label>
                 <input
@@ -99,9 +182,9 @@ const Register: FC<{}> = () => {
                   className="w-full rounded-md border-gray-300 placeholder:text-gray-300"
                 />
               </div>
-              <div className="w-[36.7rem] my-6">
+              <div className="w-[36.7rem] my-4">
                 <label
-                  className="block text-gray-500 mb-3"
+                  className="block text-gray-500 mb-2"
                   htmlFor="confirmPassword"
                 >
                   Confirm password
@@ -115,8 +198,27 @@ const Register: FC<{}> = () => {
                   className="w-full rounded-md border-gray-300 placeholder:text-gray-300"
                 />
               </div>
-              <div className="w-[36.7rem] my-6">
-                <Button type="primary" text="Create account" />
+              <div className="w-[36.7rem] mt-6 mb-4">
+                <Button
+                  type="primary"
+                  onClick={handleSubmission}
+                  text={isLoading ? "Creating..." : "Create account"}
+                />
+              </div>
+              <div className="w-[36.7rem] my-4">
+                <div
+                  className="w-full border border-gray-300 text-gray-600 rounded-md text-base font-normal px-8 py-2.5 hover:text-gray-700 transition-all duration-300 flex justify-center items-center gap-3 hover:border-gray-400 hover:cursor-pointer"
+                  onClick={handleClick}
+                >
+                  <FcGoogle size={25} />
+                  <span className="text-base">Sign up with google</span>
+                </div>
+              </div>
+              <div className="w-[36.7rem] my-4">
+                <div className="w-full border border-gray-300 text-gray-600 rounded-md text-base font-normal px-8 py-2.5 hover:text-gray-700 transition-all duration-300 flex justify-center items-center gap-3 hover:border-gray-400 hover:cursor-pointer">
+                  <BsLinkedin size={23} fill="#0077B7" className="" />
+                  <span className="text-base">Sign up with Linkedin</span>
+                </div>
               </div>
             </div>
           </form>
