feb8bd72a9dea17393ae0184c9bb69e6c9943800
Safoah91 <lampteyanastasia@gmail.com>
2023-06-21 18:43:38 +0000

    edit button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import { ButtonProps } from "./type";
 
-const Button = ({ text, type, link }: ButtonProps) => {
+const Button = ({ text, type, link, onClick }: ButtonProps) => {
   return (
     <>
       {type === "primary" ? (
-        <button className="w-full border  bg-blue500 text-white rounded-md text-base font-bold px-8 py-2.5   hover:text-gray-300 hover:bg-blue500/90 transition-all duration-300">
+        <button
+          onClick={onClick}
+          className="w-full border bg-blue500 text-white rounded-md text-base font-bold px-8 py-2.5 hover:bg-blue500/90 transition-all duration-300"
+        >
           {text}
         </button>
       ) : type === "secondary" ? (
--- src/components/button/type.d.ts
+++ src/components/button/type.d.ts
@@ -2,6 +2,7 @@ export interface ButtonProps {
   text: string;
   type: ButtonTypes;
   link?: string;
+  onClick?: (e, params = {}) => void;
 }
 
 type ButtonTypes = "primary" | "secondary" | "primary-link" | "secondary-link";
