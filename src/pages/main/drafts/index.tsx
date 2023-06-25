260b6fa3809a0be8cb54f6e5e33cdf9464e754d0
Safoaho1 <lampteyanastasia@gmail.com>
2023-06-25 00:39:13 +0000

    show draft blogs

--- src/pages/main/drafts/index.tsx
+++ src/pages/main/drafts/index.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { useQuery } from 'react-query';
+import { get } from 'api';
+// import { Link } from 'react-router-dom';
+import Button from 'components/button';
+import { BsBook } from 'react-icons/bs';
 
 const Drafts = () => {
-  return <div>Drafts</div>;
+
+  const { data, isFetching } = useQuery(['listDrafts'], () =>
+    get(`/blogs/user-drafted-blogs`)
+  );
+
+  return (
+    <>
+      <div className='border border-gray-300 rounded-md px-24 py-8 space-y-8'>
+        <header>
+          <section className='flex justify-between items-center'>
+            <div>
+              <h1 className='text-4xl'>Drafts</h1>
+              <p className='text-gray-500 text-sm mt-1'>
+                List of all unpublished blog posts
+              </p>
+            </div>
+            <div>
+              <Button
+                text='Post a content'
+                type='secondary-link'
+                link='/main/blogs/create'
+              />
+            </div>
+          </section>
+        </header>
+        <section className='border border-x-gray-300 rounded-md'>
+          {isFetching ? (
+            <p>Loading...</p>
+          ) : (
+            data?.map((blog: any) => (
+              <div key={blog?._id} className='shadow-sm'>
+                <article className='py-6 px-10 max-w-2xl'>
+                  <h3 className='text-2xl'>{blog?.title}</h3>
+                  <p className='flex items-center gap-1 text-gray-400 text-xs mt-1'>
+                    <BsBook />
+                    <span>{blog?.readTime} read</span>
+                  </p>
+                  <p className='text-gray-500 mt-3'>{blog?.except}</p>
+                </article>
+              </div>
+            ))
+          )}
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Drafts;
