5a1894a36ae0be8004b7b32c07c88741f342f53e
Safoaho1 <lampteyanastasia@gmail.com>
2023-06-25 00:35:48 +0000

    edit button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,38 +1,43 @@
-import { Link } from "react-router-dom";
-import { ButtonProps } from "./type";
+import { Link } from 'react-router-dom';
+import { ButtonProps } from './type';
 
 const Button = ({ text, type, link, onClick }: ButtonProps) => {
   return (
     <>
-      {type === "primary" ? (
+      {type === 'primary' ? (
         <button
           onClick={onClick}
-          className="w-full border bg-blue500 text-white rounded-md text-base font-bold px-8 py-2.5 hover:bg-blue500/90 transition-all duration-300"
+          type='submit'
+          className='w-full border bg-blue500 text-white rounded-md text-base font-bold px-8 py-2.5 hover:bg-blue500/90 transition-all duration-300'
         >
           {text}
         </button>
-      ) : type === "secondary" ? (
-        <button className="w-full border border-blue500 rounded-md text-base font-bold px-8 py-2.5 hover:bg-gray-100 text-blue500 transition-all duration-300">
+      ) : type === 'secondary' ? (
+        <button
+          onClick={onClick}
+          type='submit'
+          className='w-full border border-blue500 rounded-md text-base font-bold px-8 py-2.5 hover:bg-gray-100 text-blue500 transition-all duration-300'
+        >
           {text}
         </button>
-      ) : type === "primary-link" ? (
+      ) : type === 'primary-link' ? (
         <Link
           to={`${link}`}
-          className="border border-blue500 bg-blue500 text-white rounded-md text-base font-bold px-8 py-2.5 hover:border-blue500 hover:bg-white hover:text-gray-700 transition-all duration-300"
+          className='border border-blue500 bg-blue500 text-white rounded-md text-base font-bold px-8 py-2.5 hover:border-blue500 hover:bg-white hover:text-gray-700 transition-all duration-300'
         >
           {text}
         </Link>
-      ) : type === "secondary-link" ? (
+      ) : type === 'secondary-link' ? (
         <Link
           to={`${link}`}
-          className="border border-blue500 rounded-md text-base font-bold px-8 py-2.5 hover:bg-blue500 hover:text-white transition-all duration-300"
+          className='border text-blue500 border-blue500 rounded-md text-base font-bold px-8 py-2.5 hover:bg-blue500 hover:text-white transition-all duration-300'
         >
           {text}
         </Link>
       ) : (
         <Link
           to={`${link}`}
-          className="border border-blue500 bg-blue500 text-white rounded-md text-base font-bold px-8 py-2.5 hover:border-blue500 hover:bg-white hover:text-gray-700 transition-all duration-300"
+          className='border border-blue500 bg-blue500 text-white rounded-md text-base font-bold px-8 py-2.5 hover:border-blue500 hover:bg-white hover:text-gray-700 transition-all duration-300'
         >
           {text}
         </Link>
