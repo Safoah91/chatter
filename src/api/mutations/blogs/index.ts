4de0a81e6368c2551e5a7eda3be0b7b610ffb77f
Safoaho1 <lampteyanastasia@gmail.com>
2023-06-28 17:55:47 +0000

    create and update endpoints

--- src/api/mutations/blogs/index.ts
+++ src/api/mutations/blogs/index.ts
@@ -1,8 +1,8 @@
 import { post, put } from "../..";
 
 export const createBlog = async (body: any) => {
-  return await post("blogs/create", { ...body });
+  return await post("blogs/blog/create", { ...body });
 };
-export const updateBlog = async (body: any) => {
-  return await put("blog/update", { ...body });
+export const updateBlog = async (id: any, body: any) => {
+  return await put(`blogs/blog/update/?id=${id}`, { ...body });
 };
