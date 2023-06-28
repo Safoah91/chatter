cba506df0e36aad65342190ebc273915a310f229
Safoaho1 <lampteyanastasia@gmail.com>
2023-06-28 17:56:27 +0000

    single blog endpoint

--- src/api/queries/index.ts
+++ src/api/queries/index.ts
@@ -1,5 +1,14 @@
 import { get } from "api"
 
+export const getSingleBlog = async(config = {}) => {
+    try {
+        const response = await get('/blogs/blog/', {...config})
+        return response
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 export const getSingleUserBlog = async(config = {}) => {
     try {
         const response = await get('/blogs/user-blogs/blog/', {...config})
